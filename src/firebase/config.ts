e7ef09e926308abf65d151b37e9d04ee68ad1b33
Krupa Vijaypur <krupavijaypur@gmail.com>
2026-01-23 15:03:01 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2385209000-4a98f",
+  "appId": "1:59305272287:web:84d67c4d50c4251da91a6a",
+  "apiKey": "AIzaSyCRL6Ov0mfdWlpnK87LwqG85n9eKj5i4h4",
+  "authDomain": "studio-2385209000-4a98f.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "59305272287"
+};
